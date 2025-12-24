aac18dfd6a90479ed4202d90e6b27e954ca65d39
Samudra Faris <samudrafaris@gmail.com>
2025-12-24 20:59:10 +0700

    change snow globe explanation

--- src/data/showcases.ts
+++ src/data/showcases.ts
@@ -12,7 +12,7 @@ export const showcases: Showcase[] = [
     title: "Snow Globe",
     url: "/showcases/snow-globe",
     description:
-      "An interactive 3D snow globe experience built with Three.js and React Three Fiber. Users can customize their snow globe with different scenes and effects.",
-    technologies: ["Three.js", "React Three Fiber"],
+      "An interactive 3D snow globe experience built with Three.js and React Three Fiber. Users can interact with the globe, pushing the button to shake the snow and enjoy the festive scene inside.",
+    technologies: ["Three.js", "React Three Fiber", "Blender"],
   },
 ];
