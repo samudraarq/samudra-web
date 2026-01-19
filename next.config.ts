8071cd7236fd54a6754c32d23f4bfe19eb85ebd5
Samudra Faris <samudrafaris@gmail.com>
2026-01-19 18:08:56 +0700

    use turbopack again

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,29 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["three"],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      use: ["raw-loader"],
-    });
-    return config;
+  turbopack: {
+    rules: {
+      "*.glsl": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+      "*.vs": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+      "*.fs": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+      "*.vert": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+      "*.frag": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
   },
 };
 
